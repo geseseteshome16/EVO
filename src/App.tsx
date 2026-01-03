b6059376de6da004d21607e2d32265758830be77
geseseteshome16 <geseseteshome16@gmail.com>
2026-01-03 14:26:34 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   const renderPage = () => {
     switch (currentPage) {
-      case 'home':
+      case 'homee':
         return <Home onNavigate={setCurrentPage} />;
       case 'about':
         return <About />;
